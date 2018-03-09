ca38924accbbacab49538b3edc9c15ec667e2ab4
LiBai666 <overwall2016@gmail.com>
2018-03-09 14:45:49 +0800

    add template

--- csforum/boards/views.py
+++ csforum/boards/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello, world!')
+	#why does it require request param?
+    return render(request,'index.html',{"name":"saltfish","learnning":{"python":1,"js":2}})
